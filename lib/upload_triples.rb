21faf22fd65316c1a35e38604b680b3d7006ef90
Daniel Hernández <daniel@degu.cl>
2010-05-31 14:18:25 -0400

    Adding some comments

--- lib/upload_triples.rb
+++ lib/upload_triples.rb
@@ -1,3 +1,24 @@
+#!/usr/bin/ruby
+
+# This script is part of SEIA Data, A set of tools to manage
+# information in http://www.e-seia.cl, a chilean goverment web site.
+#
+# SEIA Data is copyrighted free software by Daniel Hernandez
+# (http://www.molinoverde.cl): you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SEIA Data is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SEIA Data. If not, see <http://www.gnu.org/licenses/>.
+
+# TODO: Description of that this script do.
+
 require 'rubygems'
 require 'rest_client'
 require 'optparse'
